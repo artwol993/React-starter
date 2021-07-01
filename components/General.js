b3752fc40f972d1693ece707ce4d6d17c88bf127
artwol993 <artwol993@gmail.com>
2021-07-01 23:43:31 +0200

    clean code

--- components/General.js
+++ components/General.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import "../sass/general.scss";
 import { Unigrid } from "../unigrid";
-// import { UnigridExamples } from "../examples/UnigridExamples"
-
-
-
-
 
 function General() {
     return (
         <div>
             <div className="container">Hello React</div>
             <Unigrid />
-            {/* <UnigridExamples /> */}
         </div>
 
     )
--- src/helpers.js
+++ src/helpers.js
@@ -1,275 +1,251 @@
-/*
-Copyright (c) 2018, Grzegorz Junka
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
+// import React from 'react';
+// import { getIterator } from './iterators';
+// import { UnigridRow } from './UnigridRow';
+// import { UnigridSection } from './UnigridSection';
 
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
+// let NODE_ENV = typeof process !== 'undefined' ? process.env.NODE_ENV : 'development';
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+// // *** Utility functions ***
 
-import React from 'react';
-import { getIterator } from './iterators';
-import { UnigridRow } from './UnigridRow';
-import { UnigridSection } from './UnigridSection';
+// export const isDefined = (obj, prop) => {
+//   var undefined; // really undefined
+//   return !!obj && typeof obj === 'object' && obj.hasOwnProperty(prop) && obj[prop] !== undefined;
+// };
 
-let NODE_ENV = typeof process !== 'undefined' ? process.env.NODE_ENV : 'development';
+// export const cleanProps = (props) => {
+//   const { table, data, item, box, sectionCounter, cellTypes, amend, treeAmend,
+//     condition, fromProperty, process, select, section, cells,
+//     renderAs, rowAs, mixIn, $do, children, bindToElement, ...other } = props;
+//   return other;
+// }
 
-// *** Utility functions ***
+// export const cleanCellProps = (props) => {
+//   const { cell, show, item, box, renderAs, rowAs, amend, bindToCell, treeAmend, Tx,
+//     ...other } = props;
+//   return other;
+// };
 
-export const isDefined = (obj, prop) => {
-  var undefined; // really undefined
-  return !!obj && typeof obj === 'object' && obj.hasOwnProperty(prop) && obj[prop] !== undefined;
-};
+// export const idMaker = function* () {
+//   let index = 0;
+//   while (true) yield index++;
+// }
 
-export const cleanProps = (props) => {
-  const { table, data, item, box, sectionCounter, cellTypes, amend, treeAmend,
-    condition, fromProperty, process, select, section, cells,
-    renderAs, rowAs, mixIn, $do, children, bindToElement, ...other } = props;
-  return other;
-}
+// // *** Processing expression objects ***
 
-export const cleanCellProps = (props) => {
-  const { cell, show, item, box, renderAs, rowAs, amend, bindToCell, treeAmend, Tx,
-    ...other } = props;
-  return other;
-};
+// function _applyAmend(cfg, item, box, fun) {
+//   return Object.assign({}, cfg, fun(cfg, item, box));
+// }
 
-export const idMaker = function* () {
-  let index = 0;
-  while (true) yield index++;
-}
+// function _amend(cfg, expr, item, box, how, def) {
+//   if (typeof (how) === 'function') {
+//     if (expr === def || !expr) {
+//       return _applyAmend(cfg, item, box, how);
+//     }
+//   } else if (expr && isDefined(how, expr)) {
+//     return _applyAmend(cfg, item, box, how[expr]);
+//   }
+//   return cfg;
+// }
 
-// *** Processing expression objects ***
+// export const tryAmend = (pCfg, pItem, pBox, pExpr, pDef) => {
+//   if (isDefined(pCfg, 'amend')) {
+//     return _amend(pCfg, pExpr, pItem, pBox, pCfg.amend, pDef);
+//   } else if (isDefined(pCfg, 'treeAmend')) {
+//     return _amend(pCfg, pExpr, pItem, pBox, pCfg.treeAmend, pDef);
+//   }
+//   return pCfg;
+// }
 
-function _applyAmend(cfg, item, box, fun) {
-  return Object.assign({}, cfg, fun(cfg, item, box));
-}
+// // *** Unigrid rendering functions ***
 
-function _amend(cfg, expr, item, box, how, def) {
-  if (typeof (how) === 'function') {
-    if (expr === def || !expr) {
-      return _applyAmend(cfg, item, box, how);
-    }
-  } else if (expr && isDefined(how, expr)) {
-    return _applyAmend(cfg, item, box, how[expr]);
-  }
-  return cfg;
-}
+// export const prepAmend = (iCfg, iItem, iBox, expr) => {
+//   if (isDefined(iCfg, expr)) {
+//     const aCfg = tryAmend(iCfg, iItem, iBox, expr);
+//     if (isDefined(aCfg, expr)) {
+//       return aCfg;
+//     }
+//   }
+//   return false;
+// }
 
-export const tryAmend = (pCfg, pItem, pBox, pExpr, pDef) => {
-  if (isDefined(pCfg, 'amend')) {
-    return _amend(pCfg, pExpr, pItem, pBox, pCfg.amend, pDef);
-  } else if (isDefined(pCfg, 'treeAmend')) {
-    return _amend(pCfg, pExpr, pItem, pBox, pCfg.treeAmend, pDef);
-  }
-  return pCfg;
-}
+// function _isSupported(elem) {
+//   const isUnigrid = elem.type && elem.type.isUnigrid && elem.type.isUnigrid();
+//   /*
+//   // Maybe once react supports returning multiple children from a render function
+//   acc.push(<Unigrid table={nCfg} data={data} item={item} box={this.props.box}
+//   cellTypes={this.props.cellTypes} isChildUnigrid={true} />);
+//   */
+//   return !isUnigrid;
+// }
 
-// *** Unigrid rendering functions ***
+// function _processChild(child, props) {
+//   // Pass null and undefined to React
+//   if (!child) {
+//     return child;
+//   }
 
-export const prepAmend = (iCfg, iItem, iBox, expr) => {
-  if (isDefined(iCfg, expr)) {
-    const aCfg = tryAmend(iCfg, iItem, iBox, expr);
-    if (isDefined(aCfg, expr)) {
-      return aCfg;
-    }
-  }
-  return false;
-}
+//   let binds = child.props.bindToElement || [];
+//   binds = typeof (binds) === 'string' ? [binds] : binds;
+//   let toAdd = [];
+//   for (let i = 0; i < binds.length; i++) {
+//     let funName = binds[i];
+//     let oldFun = child.props[funName];
+//     if (oldFun !== undefined) {
+//       let newFun = function () {
+//         return oldFun.apply(this.unigridElement, arguments);
+//       }
+//       toAdd.push(newFun);
+//       props[funName] = newFun.bind(newFun);
+//     }
+//   }
+//   let component = React.cloneElement(child, props);
+//   for (let i = 0; i < toAdd.length; i++) {
+//     toAdd[i].unigridElement = component;
+//   }
+//   return component;
+// }
 
-function _isSupported(elem) {
-  const isUnigrid = elem.type && elem.type.isUnigrid && elem.type.isUnigrid();
-  /*
-  // Maybe once react supports returning multiple children from a render function
-  acc.push(<Unigrid table={nCfg} data={data} item={item} box={this.props.box}
-  cellTypes={this.props.cellTypes} isChildUnigrid={true} />);
-  */
-  return !isUnigrid;
-}
+// function _getChildren(cfg, box, counter, data, item, cTypes) {
+//   let props = {
+//     box: box, data: data, item: item, cellTypes: cTypes,
+//     sectionCounter: counter, key: counter.next().value
+//   };
+//   if (isDefined(cfg, 'treeAmend')) {
+//     Object.assign(props, { treeAmend: cfg.treeAmend });
+//   }
+//   if (isDefined(cfg, 'renderAs')) {
+//     Object.assign(props, { renderAs: cfg.renderAs });
+//   }
+//   return React.Children.map(cfg.children, function (child) {
+//     return _processChild(child, props);
+//   });
+// }
 
-function _processChild(child, props) {
-  // Pass null and undefined to React
-  if (!child) {
-    return child;
-  }
+// function _shouldRender(condition, item) {
+//   const exists = isDefined(condition, 'property')
+//     && isDefined(item, condition.property);
 
-  let binds = child.props.bindToElement || [];
-  binds = typeof (binds) === 'string' ? [binds] : binds;
-  let toAdd = [];
-  for (let i = 0; i < binds.length; i++) {
-    let funName = binds[i];
-    let oldFun = child.props[funName];
-    if (oldFun !== undefined) {
-      let newFun = function () {
-        return oldFun.apply(this.unigridElement, arguments);
-      }
-      toAdd.push(newFun);
-      props[funName] = newFun.bind(newFun);
-    }
-  }
-  let component = React.cloneElement(child, props);
-  for (let i = 0; i < toAdd.length; i++) {
-    toAdd[i].unigridElement = component;
-  }
-  return component;
-}
+//   switch (condition.ifDoes) {
+//     case 'exist':
+//       return exists;
+//     case 'notExist':
+//       return !exists;
+//     case 'equal':
+//       return exists
+//         && isDefined(condition, 'value')
+//         && item[condition.property] === condition.value;
+//     case 'notEqual':
+//       return !exists
+//         || !isDefined(condition, 'value')
+//         || item[condition.property] !== condition.value;
+//   }
+//   return true;
+// }
 
-function _getChildren(cfg, box, counter, data, item, cTypes) {
-  let props = {
-    box: box, data: data, item: item, cellTypes: cTypes,
-    sectionCounter: counter, key: counter.next().value
-  };
-  if (isDefined(cfg, 'treeAmend')) {
-    Object.assign(props, { treeAmend: cfg.treeAmend });
-  }
-  if (isDefined(cfg, 'renderAs')) {
-    Object.assign(props, { renderAs: cfg.renderAs });
-  }
-  return React.Children.map(cfg.children, function (child) {
-    return _processChild(child, props);
-  });
-}
+// function addRows(cfg, box, props, counter, acc, data, item) {
+//   let aCfg = prepAmend(cfg, item, box, 'condition');
+//   if (aCfg) {
+//     if (!_shouldRender(aCfg.condition, item)) return;
+//   }
 
-function _shouldRender(condition, item) {
-  const exists = isDefined(condition, 'property')
-    && isDefined(item, condition.property);
+//   aCfg = prepAmend(cfg, item, box, 'fromProperty');
+//   if (aCfg) {
+//     const { condition, fromProperty, ...nCfg } = aCfg;
+//     const nData = item[aCfg.fromProperty];
 
-  switch (condition.ifDoes) {
-    case 'exist':
-      return exists;
-    case 'notExist':
-      return !exists;
-    case 'equal':
-      return exists
-        && isDefined(condition, 'value')
-        && item[condition.property] === condition.value;
-    case 'notEqual':
-      return !exists
-        || !isDefined(condition, 'value')
-        || item[condition.property] !== condition.value;
-  }
-  return true;
-}
+//     if (NODE_ENV !== 'production') {
+//       if (!nData || typeof (nData) !== 'object') {
+//         throw new Error(`Invalid value supplied to "fromProperty": ${aCfg.fromProperty}. ` +
+//           'The property could not be found in the data supplied to Unigrid. ' +
+//           'Consider adding the "ifDoes" exist condition.');
+//       }
+//     }
 
-function addRows(cfg, box, props, counter, acc, data, item) {
-  let aCfg = prepAmend(cfg, item, box, 'condition');
-  if (aCfg) {
-    if (!_shouldRender(aCfg.condition, item)) return;
-  }
+//     addChildren(nCfg, box, props, counter, acc, nData, undefined);
+//     return;
+//   }
 
-  aCfg = prepAmend(cfg, item, box, 'fromProperty');
-  if (aCfg) {
-    const { condition, fromProperty, ...nCfg } = aCfg;
-    const nData = item[aCfg.fromProperty];
+//   aCfg = prepAmend(cfg, item, box, 'process');
+//   if (aCfg) {
+//     const { condition, fromProperty, process, ...nCfg } = aCfg;
+//     const nData = aCfg.process(data, box);
 
-    if (NODE_ENV !== 'production') {
-      if (!nData || typeof (nData) !== 'object') {
-        throw new Error(`Invalid value supplied to "fromProperty": ${aCfg.fromProperty}. ` +
-          'The property could not be found in the data supplied to Unigrid. ' +
-          'Consider adding the "ifDoes" exist condition.');
-      }
-    }
+//     if (NODE_ENV !== 'production') {
+//       if (!nData || typeof (nData) !== 'object') {
+//         throw new Error('Invalid data returned from the "process" function.');
+//       }
+//     }
 
-    addChildren(nCfg, box, props, counter, acc, nData, undefined);
-    return;
-  }
+//     addChildren(nCfg, box, props, counter, acc, nData, undefined);
+//     return;
+//   }
 
-  aCfg = prepAmend(cfg, item, box, 'process');
-  if (aCfg) {
-    const { condition, fromProperty, process, ...nCfg } = aCfg;
-    const nData = aCfg.process(data, box);
+//   aCfg = prepAmend(cfg, item, box, 'select');
+//   if (aCfg) {
+//     const { condition, fromProperty, process, select, ...nCfg } = aCfg;
+//     executeSelect(nCfg, box, props, counter, acc, aCfg.select, data);
+//     return;
+//   }
 
-    if (NODE_ENV !== 'production') {
-      if (!nData || typeof (nData) !== 'object') {
-        throw new Error('Invalid data returned from the "process" function.');
-      }
-    }
+//   aCfg = prepAmend(cfg, item, box, 'section');
+//   if (aCfg) {
+//     const { condition, fromProperty, process, select, section, ...nCfg } = aCfg;
+//     acc.push(UnigridSection.create(nCfg, box, props, counter, aCfg.section, data, item));
+//     return;
+//   }
 
-    addChildren(nCfg, box, props, counter, acc, nData, undefined);
-    return;
-  }
+//   const cTypes = props.cellTypes;
+//   aCfg = prepAmend(cfg, item, box, 'cells');
+//   if (aCfg) {
+//     const { condition, fromProperty, process, select, section, children, ...nCfg } = aCfg;
+//     const nId = counter.next().value;
+//     const key = isDefined(item, '_unigridId') ? `${item._unigridId}-${nId}` : nId;
+//     acc.push(UnigridRow.create(Object.assign(nCfg, { box, item, cellTypes: cTypes, key })));
+//   }
 
-  aCfg = prepAmend(cfg, item, box, 'select');
-  if (aCfg) {
-    const { condition, fromProperty, process, select, ...nCfg } = aCfg;
-    executeSelect(nCfg, box, props, counter, acc, aCfg.select, data);
-    return;
-  }
+//   aCfg = prepAmend(cfg, item, box, '$do');
+//   if (aCfg) {
+//     const addProp = isDefined(aCfg, 'treeAmend') ?
+//       { treeAmend: aCfg.treeAmend } : undefined;
+//     for (let i of aCfg.$do) {
+//       let nCfg = addProp ? Object.assign({}, addProp, i) : i;
+//       addChildren(nCfg, box, props, counter, acc, data, item);
+//     }
+//   }
 
-  aCfg = prepAmend(cfg, item, box, 'section');
-  if (aCfg) {
-    const { condition, fromProperty, process, select, section, ...nCfg } = aCfg;
-    acc.push(UnigridSection.create(nCfg, box, props, counter, aCfg.section, data, item));
-    return;
-  }
+//   const children = _getChildren(cfg, box, counter, data, item, cTypes) || [];
+//   for (const i of children) {
+//     if (_isSupported(i)) {
+//       acc.push(i);
+//     } else {
+//       addChildren(i.props, box, props, counter, acc, data, item);
+//     }
+//   }
+// }
 
-  const cTypes = props.cellTypes;
-  aCfg = prepAmend(cfg, item, box, 'cells');
-  if (aCfg) {
-    const { condition, fromProperty, process, select, section, children, ...nCfg } = aCfg;
-    const nId = counter.next().value;
-    const key = isDefined(item, '_unigridId') ? `${item._unigridId}-${nId}` : nId;
-    acc.push(UnigridRow.create(Object.assign(nCfg, { box, item, cellTypes: cTypes, key })));
-  }
+// function executeSelect(cfg, box, props, counter, acc, select, data) {
+//   for (let i of getIterator(data, select)) {
+//     addRows(cfg, box, props, counter, acc, data, i);
+//   }
+// }
 
-  aCfg = prepAmend(cfg, item, box, '$do');
-  if (aCfg) {
-    const addProp = isDefined(aCfg, 'treeAmend') ?
-      { treeAmend: aCfg.treeAmend } : undefined;
-    for (let i of aCfg.$do) {
-      let nCfg = addProp ? Object.assign({}, addProp, i) : i;
-      addChildren(nCfg, box, props, counter, acc, data, item);
-    }
-  }
+// function addChildren(cfg, box, props, counter, acc, data, item) {
+//   if (item === undefined) {
+//     executeSelect(cfg, box, props, counter, acc, 'first', data);
+//   } else {
+//     addRows(cfg, box, props, counter, acc, data, item);
+//   }
+// }
 
-  const children = _getChildren(cfg, box, counter, data, item, cTypes) || [];
-  for (const i of children) {
-    if (_isSupported(i)) {
-      acc.push(i);
-    } else {
-      addChildren(i.props, box, props, counter, acc, data, item);
-    }
-  }
-}
+// export const createChildren = (cfg, box, props, counter, data, item) => {
+//   const acc = [];
+//   addChildren(cfg, box, props, counter, acc, data, item);
+//   return acc;
+// }
 
-function executeSelect(cfg, box, props, counter, acc, select, data) {
-  for (let i of getIterator(data, select)) {
-    addRows(cfg, box, props, counter, acc, data, i);
-  }
-}
-
-function addChildren(cfg, box, props, counter, acc, data, item) {
-  if (item === undefined) {
-    executeSelect(cfg, box, props, counter, acc, 'first', data);
-  } else {
-    addRows(cfg, box, props, counter, acc, data, item);
-  }
-}
-
-export const createChildren = (cfg, box, props, counter, data, item) => {
-  const acc = [];
-  addChildren(cfg, box, props, counter, acc, data, item);
-  return acc;
-}
-
-export const newChildren = (cfg, box, props, data, item) => {
-  const counter = idMaker();
-  return createChildren(cfg, box, props, counter, data, item);
-}
+// export const newChildren = (cfg, box, props, data, item) => {
+//   const counter = idMaker();
+//   return createChildren(cfg, box, props, counter, data, item);
+// }
